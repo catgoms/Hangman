92e9456059638e9ce72de1374f50bda38852ffe6
Soo Bin Lee <mariddo3sb@gmail.com>
2023-12-09 15:04:37 +1000

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] words = new String[] {
+                "apple",
+                "dictionary",
+                "television",
+                "cushion",
+                "smartphone",
+                "puzzle"
+            };
+
+            Console.WriteLine("Let's play a Hangman Game!\n");
+
+            String chosenWord = "apple";
+
+            int n = 0;
+
+            while(n < chosenWord.Length) {
+                Console.Write(" _");
+                n++;
+            }
+
+            Console.WriteLine("Type the alphabet or the word for a guess: ");
+            String guess = Console.ReadLine();
+
+            if (guess.Length > 1) {
+
+            }
+
+            if (chosenWord.Contains(guess)) {
+                int index = chosenWord.IndexOf(guess);
+            }
+
+
+            Console.WriteLine("\n");
+        }
+
+        static void GameSetting()
+        {
+            
+
+
+        }
+    }
+}
