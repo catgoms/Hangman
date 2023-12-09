4ee614af450a376af75a4323ffc8642f50eefd87
Soo Bin Lee <mariddo3sb@gmail.com>
2023-12-09 15:26:14 +1000

    structuring the main game

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -19,30 +19,40 @@ namespace Hangman
 
             String chosenWord = "apple";
 
-            int n = 0;
-
-            while(n < chosenWord.Length) {
-                Console.Write(" _");
-                n++;
-            }
-
-            Console.WriteLine("Type the alphabet or the word for a guess: ");
-            String guess = Console.ReadLine();
-
-            if (guess.Length > 1) {
-
-            }
-
-            if (chosenWord.Contains(guess)) {
-                int index = chosenWord.IndexOf(guess);
-            }
+            HangmanGame(chosenWord);
 
 
             Console.WriteLine("\n");
         }
 
-        static void GameSetting()
+        static void HangmanGame(String chosenWord)
         {
+            String[] wordDisplay = new string[] { };
+
+            for(int i = 0; i < chosenWord.Length; i++) {
+                wordDisplay[i] = " _";
+                Console.Write(wordDisplay[i]);
+            }
+
+            bool guessed = false;
+
+            while(!guessed) {
+                Console.WriteLine("\nType the alphabet or the word for a guess: ");
+                String guess = Console.ReadLine();
+
+                if (guess.Length > 1 && chosenWord == guess) {
+                    Console.WriteLine("Correct!");
+                    Console.WriteLine("Do you want to play the game again? (Y/N): ");
+                    String restart = Console.ReadLine();
+                    guessed = true;
+                } else if (guess.Length == 1 && chosenWord.Contains(guess)) {
+                    int index = chosenWord.IndexOf(guess);
+                    wordDisplay[index] = " " + guess;
+                } else {
+                    // draw hangman
+                }
+            }
+
             
 
 
